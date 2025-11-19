1e21f8d4470b3dc98a45ca742a40e02bf4f0c60f
sukrosono <goendoell49@gmail.com>
2025-11-19 13:08:49 +0700

    Chore: unpack from spring initializer.

---
+++ sukrosono/src/main/java/com/allo/sukrosono/SukrosonoApplication.java
@@ -0,0 +1,13 @@
+package com.allo.sukrosono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukrosonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SukrosonoApplication.class, args);
+	}
+
+}
