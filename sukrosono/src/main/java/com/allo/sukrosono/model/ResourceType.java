95c8db9cc6d609d542b43bbf8e34ff780dda3d25
sukrosono <goendoell49@gmail.com>
2025-11-23 04:46:05 +0700

    Chore: drop unused field, fix typo.

--- sukrosono/src/main/java/com/allo/sukrosono/model/ResourceType.java
+++ sukrosono/src/main/java/com/allo/sukrosono/model/ResourceType.java
@@ -2,13 +2,10 @@ package com.allo.sukrosono.model;
 
 public enum ResourceType {
     LATEST_IDR_RATES("latest_idr_rates"),
-    HISTORYCAL_IDR_USD("historical_idr_usd"),
+    HISTORICAL_IDR_USD("historical_idr_usd"),
     SUPPORTED_CURRENCIES("supported_currencies");
 
-    private String action;
-
     ResourceType(String name) {
-        action = name;
+        name = name;
     }
-
 }
