d6bd3c3e6241b0c99a46253c41f279a80c33aa56
sukrosono <goendoell49@gmail.com>
2025-11-24 00:14:15 +0700

    Fix: unparsed property with @JsonProperty annotation.

--- sukrosono/src/main/java/com/allo/sukrosono/model/TimeSeriesRatesResponse.java
+++ sukrosono/src/main/java/com/allo/sukrosono/model/TimeSeriesRatesResponse.java
@@ -1,7 +1,9 @@
 package com.allo.sukrosono.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Map;
 
-public record TimeSeriesRatesResponse(int amount, String base, String startDate,
-                                      String endDate, Map<String, Double> rates) {
+public record TimeSeriesRatesResponse(int amount, String base, @JsonProperty("start_date") String startDate,
+                                      @JsonProperty("end_date") String endDate, Map<String, Map<String, Double>> rates) {
 }
